dbb652cc15987e8104400c72eb5dbf09013208ac
Vladimir Nikonov <olvnikon@gmail.com>
2017-03-06 16:31:08 +0100

    * Added "id" to Course entity. * Added outputs to CourseComponent.

--- src/app/core/entities/course.ts
+++ src/app/core/entities/course.ts
@@ -1,4 +1,5 @@
 export class Course {
+  public id: number;
   public name: string;
   public duration: string;
   public date: Date;
--- src/app/pages/course-list/course/course.component.ts
+++ src/app/pages/course-list/course/course.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Course } from '../../../core/entities';
 
 @Component({
@@ -9,4 +9,14 @@ import { Course } from '../../../core/entities';
 })
 export class CourseComponent {
   @Input() private course: Course;
+  @Output() private deleteCourse = new EventEmitter();
+  @Output() private editCourse = new EventEmitter();
+
+  private onDeleteCourse() {
+    this.deleteCourse.emit({ course: this.course });
+  }
+
+  private onEditCourse() {
+    this.editCourse.emit({ course: this.course });
+  }
 }
